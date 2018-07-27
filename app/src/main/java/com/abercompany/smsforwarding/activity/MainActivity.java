24bebb07abd33afeababedf34ddfdd97b376b5fa
Jin <s.conast@gmail.com>
2018-07-27 19:50:07 +0900

    하단 메뉴 NavBar 로 바꾸기

--- app/src/main/java/com/abercompany/smsforwarding/activity/MainActivity.java
+++ app/src/main/java/com/abercompany/smsforwarding/activity/MainActivity.java
@@ -8,12 +8,19 @@ import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -55,7 +62,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private String TAG = MainActivity.class.getSimpleName();
 
@@ -78,12 +85,14 @@ public class MainActivity extends AppCompatActivity {
 
     private BuildingAdapter buildingAdapter;
 
+    private Menu menu;
+    private MenuItem navBuilding, navNewData, navExistingData, navSetting;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setMain(this);
 
         PermissionListener permissionlistener = new PermissionListener() {
             @Override
@@ -100,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
                 getResident();
                 getDefaulter();
 
+                initNav();
                 setInitFrag();
             }
 
@@ -124,64 +134,49 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().addOnBackStackChangedListener(backStackChangedListener);
     }
 
-    public void bottomBtnClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_building:
-                initNaviButton(view);
-                if (buildingFragment == null) {
-                    buildingFragment = BuildingFragment.newInstance();
-                    getBuilding();
-                }
-                switchContent(buildingFragment, "BUILDING");
-                break;
 
-            case R.id.btn_deposit_data:
-                initNaviButton(view);
-                if (newDataFragment == null) {
-                    newDataFragment = NewDataFragment.newInstance(newDatas, residents, brokers);
-                }
-                switchContent(newDataFragment, "NEW_DATA");
-                break;
+    private void initNav() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            case R.id.btn_withdraw:
-                initNaviButton(view);
-                if (existingDataFragment == null) {
-                    existingDataFragment = ExistingDataFragment.newInstance(existingDatas, residents, brokers);
-                }
-                switchContent(existingDataFragment, "EXISTING_DATA");
-                break;
 
-            case R.id.btn_room:
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-                Intent intent = new Intent(this, ContractActivity.class);
-                intent.putExtra("defaulter", (Serializable) defaulters);
-                startActivity(intent);
-                break;
+        View header = navigationView.getHeaderView(0);
 
-            case R.id.btn_setting:
-                initNaviButton(view);
-                if (settingFragment == null) {
-                    settingFragment = SettingFragment.newInstance(residents, brokers, nums, defaulters);
-                }
-                switchContent(settingFragment, "SETTING");
-                break;
-        }
+        menu = navigationView.getMenu();
+        navBuilding = menu.findItem(R.id.nav_building);
+        navNewData = menu.findItem(R.id.nav_new_data);
+        navExistingData = menu.findItem(R.id.nav_existing_data);
+        navSetting = menu.findItem(R.id.nav_setting);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     public void setInitFrag() {
         buildingFragment = BuildingFragment.newInstance();
         getSupportFragmentManager().beginTransaction().replace(R.id.container, buildingFragment, "BUILDING").addToBackStack("BUILDING").commit();
-        binding.btnBuilding.setEnabled(false);
+        navBuilding.setEnabled(false);
 
         getBuilding();
     }
 
-    private void initNaviButton(View v) {
-        binding.btnBuilding.setEnabled(true);
-        binding.btnDepositData.setEnabled(true);
-        binding.btnWithdraw.setEnabled(true);
-        binding.btnSetting.setEnabled(true);
-        v.setEnabled(false);
+    private void initNaviButton(MenuItem menu) {
+        navBuilding.setEnabled(true);
+        navNewData.setEnabled(true);
+        navExistingData.setEnabled(true);
+        navSetting.setEnabled(true);
+        menu.setEnabled(false);
     }
 
     public void switchContent(Fragment fragment, String tag) {
@@ -202,15 +197,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (getSupportFragmentManager().getBackStackEntryCount() == 1) {
-            finish();
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                MainActivity.this.finishAffinity();
-            } else {
-                System.exit(0);
-            }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            if (getSupportFragmentManager().getBackStackEntryCount() == 1){
+                finish();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                {
+                    MainActivity.this.finishAffinity();
+                }
+                else
+                {
+                    System.exit(0);
+                }
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+            else {
+                super.onBackPressed();
+            }
         }
     }
 
@@ -233,17 +238,17 @@ public class MainActivity extends AppCompatActivity {
             if (currentTab == null) return;
             switch (currentTab) {
                 case "BUILDING":
-                    initNaviButton(binding.btnBuilding);
+                    initNaviButton(navBuilding);
                     setBuildingAdapter(buildings);
                     break;
                 case "NEW_DATA":
-                    initNaviButton(binding.btnDepositData);
+                    initNaviButton(navNewData);
                     break;
                 case "EXISTING_DATA":
-                    initNaviButton(binding.btnWithdraw);
+                    initNaviButton(navExistingData);
                     break;
                 case "SETTING":
-                    initNaviButton(binding.btnSetting);
+                    initNaviButton(navSetting);
                     break;
             }
         }
@@ -563,5 +568,68 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_building) {
+            initNaviButton(item);
+            if (buildingFragment == null) {
+                buildingFragment = BuildingFragment.newInstance();
+                getBuilding();
+            }
+            switchContent(buildingFragment, "BUILDING");
+        } else if (id == R.id.nav_new_data) {
+            initNaviButton(item);
+            if (newDataFragment == null) {
+                newDataFragment = NewDataFragment.newInstance(newDatas, residents, brokers);
+            }
+            switchContent(newDataFragment, "NEW_DATA");
+        } else if (id == R.id.nav_existing_data) {
+            initNaviButton(item);
+            if (existingDataFragment == null) {
+                existingDataFragment = ExistingDataFragment.newInstance(existingDatas, residents, brokers);
+            }
+            switchContent(existingDataFragment, "EXISTING_DATA");
+        } else if (id == R.id.nav_setting) {
+            initNaviButton(item);
+            if (settingFragment == null) {
+                settingFragment = SettingFragment.newInstance(residents, brokers, nums, defaulters);
+            }
+            switchContent(settingFragment, "SETTING");
+        }
+
+
+        return true;
     }
 }
